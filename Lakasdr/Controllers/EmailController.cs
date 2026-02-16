01a9a814bbdcb5c389d73010c2a8a2d8fe363520
Márk Tóth <toth.mark0312@gmail.com>
2026-02-16 18:29:53 +0100

    email elkezdve

---
+++ Lakasdr/Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+
+public class EmailController : Controller
+{
+    [HttpPost]
+    public IActionResult SendEmail(string email)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress("Sajat App", "teemailcimed@gmail.com")); //kitol (sajat appot meg meg kell nezni mit jelent)
+        message.To.Add(new MailboxAddress("", email));  //kinek
+        message.Subject = "Teszt üzenet"; //email targya
+
+        message.Body = new TextPart("plain")
+        {
+            Text = "Szia! Ez egy teszt email az MVC alkalmazásból."
+        };
+
+        using (var client = new SmtpClient())
+        {
+            client.Connect("smtp.gmail.com", 587, false);
+            client.Authenticate("teemailcimed@gmail.com", "APP_JELSZO"); //gmail fiokban kell 2faktoros azon, majd app passwordben kod kunyi
+            client.Send(message);
+            client.Disconnect(true);
+        }
+
+        return Content("Email elküldve!");
+    }
+}
