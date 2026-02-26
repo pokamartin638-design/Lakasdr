ab54c06e33514ca0360eb80299d8593bd81d4445
Bottyán Vak <ecdl01@vbjnet.hu>
2026-02-26 10:19:12 +0100

    vmi

--- Lakasdr/Controllers/HomeController.cs
+++ Lakasdr/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lakasdr.Controllers
 {
@@ -11,11 +12,11 @@ namespace Lakasdr.Controllers
     
     public class HomeController : Controller
     {
-        private readonly WorkDbContext _db;
+        private readonly WorkDbContext _context;
         private readonly IWebHostEnvironment _env;
-        public HomeController(WorkDbContext db, IWebHostEnvironment env)
-        {
-            _db = db;
+        public HomeController(WorkDbContext context, IWebHostEnvironment env)
+        { 
+            _context = context;
             _env = env;
         }
 
@@ -61,12 +62,82 @@ namespace Lakasdr.Controllers
             HttpContext.Session.Clear();
             return RedirectToAction("Index");
         }
-//--------------------------------------------------------------------------------------------------------
-        public IActionResult Update()
+        //--------------------------------------------------------------------------------------------------------
+        public IActionResult Index1()
+        {
+            var images = _context.Images.ToList();
+            return View(images);
+        }
+
+        // UPLOAD FORM
+        [HttpGet]
+        public IActionResult Upload()
         {
             return View();
         }
-//--------------------------------------------------------------------------------------------------------
+
+        // UPLOAD POST
+        [HttpPost]
+        public IActionResult Upload(string nev, IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return View();
+
+            var allowed = new[] { ".jpg", ".jpeg", ".png" };
+            var ext = Path.GetExtension(file.FileName).ToLower();
+
+            if (!allowed.Contains(ext))
+                return View();
+
+            var fileName = Guid.NewGuid().ToString() + ext;
+            var uploadPath = Path.Combine(_env.WebRootPath, "uploads");
+
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            var fullPath = Path.Combine(uploadPath, fileName);
+
+            using (var stream = new FileStream(fullPath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            var image = new Image
+            {
+                Nev = nev,
+                FilePath = "/uploads/" + fileName,
+                UploadDate = DateTime.Now
+            };
+
+            _context.Images.Add(image);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // DELETE
+        public IActionResult Delete(int id)
+        {
+            var img = _context.Images.FirstOrDefault(x => x.Id == id);
+            if (img == null) return NotFound();
+
+            if (!string.IsNullOrEmpty(img.FilePath))
+            {
+                var physicalPath = Path.Combine(
+                    _env.WebRootPath,
+                    img.FilePath.TrimStart('/')
+                );
+
+                if (System.IO.File.Exists(physicalPath))
+                    System.IO.File.Delete(physicalPath);
+            }
+
+            _context.Images.Remove(img);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        //--------------------------------------------------------------------------------------------------------
         public IActionResult ImageUpdate()
         {
             return View();
--- Lakasdr/Models/Images.cs
+++ Lakasdr/Models/Images.cs
@@ -5,6 +5,7 @@
         {
             public int Id { get; set; }
             public string Nev { get; set; }
+            public string FilePath { get; set; }
             public DateTime UploadDate { get; set; } = DateTime.Now;
         }
     
