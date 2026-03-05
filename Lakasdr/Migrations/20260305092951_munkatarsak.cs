23306a6c876f75d5d2391a85e63ea85fdb329177
Bottyán Vak <ecdl01@vbjnet.hu>
2026-03-05 10:31:57 +0100

    munkatars_vissza_javitas

---
+++ Lakasdr/Migrations/20260305092951_munkatarsak.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lakasdr.Migrations
+{
+    /// <inheritdoc />
+    public partial class munkatarsak : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
